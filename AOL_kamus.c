d4cbdda02cf8c5ce3ece63e06c832c19bb3f8ab9
divika <divikarathi15@gmail.com>
2025-05-31 21:12:59 +0700

    Add files via upload

--- AOL_kamus.c
+++ AOL_kamus.c
@@ -12,6 +12,12 @@ typedef struct hashNode{
 
 hashNode Kamus[DICT_SIZE];
 
+//struct buat sorting
+typedef struct {
+    char key[20];
+    char kata[20];
+} entry;
+
 unsigned long hash(const char *key){
     unsigned long hash = 5381;
 
@@ -27,6 +33,7 @@ int hashToIndex(const char *key){
     return hash(key) % DICT_SIZE;
 }
 
+//insert pake linear probing
 void insertKamus(const char *key, const char *value){
     int index = hashToIndex(key);
     
@@ -40,8 +47,8 @@ void insertKamus(const char *key, const char *value){
 
 const char *searchKamus(const char *key){
     int index = hashToIndex(key);
-
     int startIndex = index;
+    
     while(strcmp(Kamus[index].key, key) != 0){
         if(Kamus[index].key[0] == '\0'){
             return NULL; 
@@ -54,23 +61,38 @@ const char *searchKamus(const char *key){
 
     return Kamus[index].kata;
 }
-void printSortedKamus() {
-    hashNode temp[DICT_SIZE];
+
+int compareEntries(const void *a, const void *b){
+    entry *entryA = (entry *)a;
+    entry *entryB = (entry *)b;
+    return strcmp(entryA->key, entryB->key);
+}
+
+void printSortedKamus(){
+    entry entries[DICT_SIZE];
     int count = 0;
 
-    for (int i = 0; i < DICT_SIZE; i++) {
-        if (Kamus[i].key[0] != '\0') {
-            temp[count++] = Kamus[i];
+    for(int i =0;i<DICT_SIZE;i++){
+        if (Kamus[i].key[0]!='\0'){
+            strcpy(entries[count].key, Kamus[i].key);
+            strcpy(entries[count].kata, Kamus[i].kata);
+            count++;
         }
     }
+    qsort(entries, count, sizeof(entry), compareentries);
+    printf("\nIsi Kamus (sorted alphabetically):\n");
+    for(int i=0; i<count; i++){
+        printf("%s -> %s\n", entries[i].key, entries[i].kata);
+    }
+}
 
-        for (int j = 0; j < count - i - 1; j++) {
-            if (strcmp(temp[j].key, temp[j + 1].key) > 0) {
-                hashNode swap = temp[j];
-                temp[j] = temp[j + 1];
-                temp[j + 1] = swap;
-            }
-        }
-    }
+int main(){
+    insertKamus ("apel", "apple");
+    printsortedkamus();
+    char nyari [20];
+
+    return 0;
+}
+
 
 
